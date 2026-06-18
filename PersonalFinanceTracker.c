971a04cd09f9c575d6d844501f296058a529a83b
Tamoghna-Pal <deep698433@gmail.com>
2026-06-18 12:39:50 +0530

    added a fitness and finance real life calculators using c language

--- PersonalFinanceTracker.c
+++ PersonalFinanceTracker.c
@@ -7,14 +7,14 @@
 #include <stdio.h>
 
 int main() {
-    // Variables for tracking finances
+    
     float income, food_expense, rent_expense, entertainment_expense;
     float total_expenses, remaining_savings, savings_percentage;
     float projected_yearly_savings;
 
     printf("=== PERSONAL FINANCE TRACKER ===\n\n");
 
-    // 1. Get User Input
+   
     printf("Enter your total monthly income: ");
     scanf("%f", &income);
 
@@ -27,13 +27,13 @@ int main() {
     printf("Enter monthly entertainment/misc expenses: ");
     scanf("%f", &entertainment_expense);
 
-    // 2. Core Calculations
+   
     total_expenses = food_expense + rent_expense + entertainment_expense;
     remaining_savings = income - total_expenses;
     savings_percentage = (remaining_savings / income) * 100.0;
     projected_yearly_savings = remaining_savings * 12.0;
 
-    // 3. Display the Financial Summary Layout
+    
     printf("\n--------------------------------");
     printf("\nMONTHLY FINANCIAL SUMMARY");
     printf("\n--------------------------------");
