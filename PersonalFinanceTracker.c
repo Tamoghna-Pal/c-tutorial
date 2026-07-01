eaf543b676596a7d55e57b041b0129c4b3836cb4
Tamoghna-Pal <deep698433@gmail.com>
2026-07-02 00:10:28 +0530

    Updated finance calculator with input validation system

--- PersonalFinanceTracker.c
+++ PersonalFinanceTracker.c
@@ -14,23 +14,51 @@ int main() {
 
     printf("=== PERSONAL FINANCE TRACKER ===\n\n");
 
-   
-    printf("Enter your total monthly income: ");
-    scanf("%f", &income);
+    //INPUT VALIDATION FOR INCOME
 
-    printf("Enter monthly food expenses: ");
-    scanf("%f", &food_expense);
+    while(1){
+printf("enter your monthly income:");
+if (scanf("%f",&income)==1 && income>=0){ //1 here means that 1 variable is successfully saved only when input is float 
+    break ;   // valid input ; exist loop
+}
+    printf("error.please provide a valid input\n");
+    while (getchar()!='\n');
+}
 
-    printf("Enter monthly rent/hostel expenses: ");
-    scanf("%f", &rent_expense);
+   while(1){
+printf("enter a valid food_expense ");
+if (scanf("%f",&food_expense)==1 && food_expense>=0){
+    break;
+}   printf("error.please provide a valid input");
+    while(getchar()!='\n');
+   }
 
-    printf("Enter monthly entertainment/misc expenses: ");
-    scanf("%f", &entertainment_expense);
+   while(1){
+    printf("enter a valid rent_expense");
+    if(scanf("%f",&rent_expense)==1 && rent_expense>=0){
+        break ;
+    } printf("error.Please provide a valid input ");
+    while(getchar()!='\n');
+   }
+    
+   while(1){
+    printf("enter a valid entertainment_expense");
+    if(scanf("%f",& entertainment_expense)==1 && entertainment_expense>=0 ){
+        break ;
+    }printf("invalid input. Please provide a valid input");
+    while(getchar()!='\n');
+}
+    
 
    
     total_expenses = food_expense + rent_expense + entertainment_expense;
     remaining_savings = income - total_expenses;
-    savings_percentage = (remaining_savings / income) * 100.0;
+
+
+    // now ensuring that its not undefined and not showing error using ternary operator caled conditional check :  { condition ? expression_if_true : expression_if_false; }
+    
+
+    savings_percentage = ( income > 0) ? (remaining_savings / income) * 100.0 : 0.0;
     projected_yearly_savings = remaining_savings * 12.0;
 
     
---
+++ python_coding.py
@@ -0,0 +1,25 @@
+'''
+this is multi line comment
+'''
+'the following is an example of concatenation i.e addition '
+list_1=[1,2,3]
+list_2=[4,5,6]
+combined_list=list_1+list_2
+print(combined_list)
+str1="i need coffee at least"
+a=2
+str2="times, while studying "
+print(str1,a,str2)
+print("hello", end=" ")
+print("world")
+'sep parameter is induced at end of each character and end character is seprated after each strings '
+'Idetifier is a name used for variables , functions , classes , ;ists and methods to identify them '
+num=10.5
+val=10
+print(num>val)
+txt=""
+print(bool(txt))
+lang="python"
+print(bool(lang))
+tree=" "
+print(bool(tree))
